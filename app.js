165afe6e31e4ed45445fe7d0951898732e8741da
p0033 <p0033@student.jamk.fi>
2025-03-27 23:17:07 +0200

    Add styles, img and functions

--- app.js
+++ app.js
@@ -5,16 +5,25 @@ const app = Vue.createApp({
       showBooks: true,
       books: [
         {
+          id: 1,
           title: "Assassin's quest",
           author: "Robin Hobb",
+          img: "assets/book1.png",
+          isFav: true,
         },
         {
+          id: 2,
           title: "Fool's errand",
           author: "Robin Hobb",
+          img: "assets/book2.png",
+          isFav: false,
         },
         {
+          id: 3,
           title: "Assassin's fate",
           author: "Robin Hobb",
+          img: "assets/book3.png",
+          isFav: true,
         },
       ],
     };
@@ -22,6 +31,10 @@ const app = Vue.createApp({
   methods: {
     toggleShowBooks() {
       this.showBooks = !this.showBooks;
+    },
+    toggleFav(book) {
+      // let book = this.books.find((book) => book.id == id);
+      book.isFav = !book.isFav;
     },
     changeTitle(title) {
       this.title = title;
@@ -31,6 +44,11 @@ const app = Vue.createApp({
       if (data) {
         console.log(data);
       }
+    },
+  },
+  computed: {
+    filteredBooks() {
+      return this.books.filter((book) => book.isFav);
     },
   },
 });
